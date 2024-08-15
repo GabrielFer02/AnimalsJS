a9c65df19dba792e3f8fbfbb36d43dfdad086909
Gabriel Fernando <gf313074@gmail.com>
2024-08-15 14:30:40 -0300

    create toolTip acton

---
+++ js/modules/tooltip.js
@@ -0,0 +1,47 @@
+export default function actionTooltip() {
+  const tooltips = document.querySelectorAll("[data-tooltip]");
+
+  if (tooltips.length) {
+    function createTooltip(element) {
+      const tooltipBox = document.createElement("div");
+      const textElement = element.getAttribute("aria-label");
+      tooltipBox.classList.add("tooltip");
+      tooltipBox.innerText = textElement;
+      document.body.appendChild(tooltipBox);
+      return tooltipBox;
+    }
+
+    function moveOnMouse(event) {
+      const tooltipBox = createTooltip(this);
+      tooltipBox.style.top = event.pageY + "px";
+      tooltipBox.style.left = event.pageX + "px";
+
+      mouseLeave.tooltipBox = tooltipBox;
+      mouseLeave.element = this;
+      this.addEventListener("mouseleave", mouseLeave);
+
+      mouseMove.tooltipBox = tooltipBox;
+      this.addEventListener("mousemove", mouseMove);
+    }
+
+    const mouseLeave = {
+      handleEvent() {
+        this.tooltipBox.remove();
+        this.element.removeEventListener("mouseleave", mouseLeave);
+        this.element.removeEventListener("mousemove", mouseMove);
+      }
+    }
+
+    const mouseMove = {
+      handleEvent(event) {
+        this.tooltipBox.style.top = event.pageY + 2 + "px";
+        this.tooltipBox.style.left = event.pageX + 2 + "px";
+      }
+    }
+
+    tooltips.forEach((item) => {
+      item.addEventListener("mouseover", moveOnMouse);
+    })
+  }
+}
+
--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ import scroll from "./modules/scroll.js";
 import sectionNavigation from "./modules/scroll-animation.js";
 import activeFaq from "./modules/faq-animation.js";
 import activeList from "./modules/list-animation.js";
+import initModal from "./modules/modal.js";
+import actionTooltip from "./modules/tooltip.js";
 
 scroll();
 sectionNavigation();
 activeFaq();
 activeList();
+initModal();
+actionTooltip();
