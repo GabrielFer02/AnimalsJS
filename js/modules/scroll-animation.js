91b2b6c397ae0c76fe3acc951a40d0e407564eac
Gabriel Fernando <gf313074@gmail.com>
2024-08-20 23:29:15 -0300

    animation numbers v2

--- js/modules/scroll-animation.js
+++ js/modules/scroll-animation.js
@@ -2,20 +2,18 @@ import initAnimationNumbers from "./animation-numbers.js";
 
 export default function sectionNavigation() {
   const section = document.querySelectorAll("[data-anime='scroll']");
-  const sectionNumbers = document.querySelector("section.numeros");
-  console.log(sectionNumbers)
+
   if (section.length) {
     function animaScroll() {
       section.forEach((item) => {
         if ((item.getBoundingClientRect().top - (window.innerHeight * 0.6)) < 0) {
           if (!item.classList.contains("ativo")) {
             item.classList.add("ativo");
-            if (sectionNumbers.classList.contains("ativo")) {
+            if (item.classList.contains("ativo") && item.classList.contains("numeros")) {
               initAnimationNumbers();
             }
           }
         }
-        
       });
     }
     animaScroll();
